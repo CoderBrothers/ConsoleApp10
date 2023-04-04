ac317f4a1f9c355b368f5f467639b698cb34c7c5
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-04-04 17:21:15 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,25 @@
+﻿interface IAnimal
+{
+    public double Speed { get; set; }
+}
+interface IPredator : IAnimal
+{
+    public void Attack(IPrey prey);
+}
+interface IPrey : IAnimal
+{
+
+}
+class Cat : IPredator
+{
+    double IAnimal.Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    void IPredator.Attack(IPrey prey)
+    {
+        throw new NotImplementedException();
+    }
+}
+class Bird : IPrey
+{
+    public double Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
